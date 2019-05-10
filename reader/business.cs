b453e08018e1b7e1d62259df070949a8810928a7
Nick Trotter <nicktrotter4@yahoo.com>
2019-05-10 09:55:46 -0400

    commit

---
+++ reader/business.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reader
+{
+    public class business
+    {
+        public string Name { get; set; }
+        public int phone { get; set; }
+        public string Category { get; set; }
+    }
+}
